7edb9230fc7aa29a35438dcf537b06a8582507b9
Hoang Tran <hoatr725@student.liu.se>
2021-04-27 01:16:45 +0200

    code cleanup

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from update_u import update_u
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def main():
     """
     Params:
@@ -21,7 +22,6 @@ def main():
     epochs = 10
     SNR = 20
     m = 10
-
 
     # img = lab4.make_circle(128,128,64)
     img = lab4.get_cameraman().astype(float)
@@ -43,7 +43,7 @@ def main():
         HL = est_HL(L)
         L = update_L(L,delta_s, D, HL)
 
-    fig,axs= plt.subplots(1, 3, constrained_layout=True)
+    fig,axs = plt.subplots(1, 3, constrained_layout=True)
     axs[0].imshow(img, cmap='gray')
     axs[0].set_title('Original image')
     axs[1].imshow(noisy_img,cmap='gray')
@@ -52,18 +52,18 @@ def main():
     axs[2].set_title('Improved noisy image')
     fig.suptitle(f"epochs = {epochs}, delta_s = {delta_s}, m = {m}", fontsize=16)
 
-    # plt.show()
+    plt.show()
     plt.close('all')
 
     """Inpainting with TV"""
-    missing_info = np.random.randint(0, high=5, size=img.shape)
-    missing_info = np.clip(missing_info,0,1)
-    img_missing = np.multiply(img, missing_info)
-
-    TV_epochs = 1000
+    TV_epochs = 1500
     alpha = 0.95
     lam = 0.01
     visualize = True
+
+    missing_info = np.random.randint(0, high=3, size=img.shape)
+    missing_info = np.clip(missing_info,0,1)
+    img_missing = np.multiply(img, missing_info)
 
     u = np.copy(img_missing)
     g = np.copy(img_missing)
@@ -74,13 +74,10 @@ def main():
         u = update_u(u, g, missing_info, alpha, lam, ksize=3, sigma=3)
         u = np.clip(u, 0, 255)
 
-        if i%10 == 0 and visualize:
+        if i % 30 == 0 and visualize:
             plt.imshow(u, cmap="gray")
             plt.title(f"iteration {i}")
             plt.pause(0.000001)
-
-
-
 
     figure1, axes1 = plt.subplots(2,2, constrained_layout=True)
     axes1[0,0].imshow(missing_info, cmap='gray')
